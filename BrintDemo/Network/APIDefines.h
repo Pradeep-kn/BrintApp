326049dfcc9e8f77a6048694b7837ae564e4653e
Tabrez <tabrez0481@gmail.com>
2014-09-26 23:27:52 +0530

    Added Base network objects and helper utility classes..

---
+++ BrintDemo/Network/APIDefines.h
@@ -0,0 +1,48 @@
+//
+//  APIDefines.h
+//  CabHoundPassenger
+//
+//  Created by Tabrez on 4/18/13.
+//  Copyright (c) 2013 Tarento technologies. All rights reserved.
+//
+
+
+
+/* Api url's */
+
+
+#define KBaseURL @"" // Production server base url
+
+
+
+#define API_SUCESS 200
+
+#define kStatusCode @"statusCode"
+
+#define kErrorMessage @"ErrorMessage"
+
+#define kStatusMessage @"StatusMessage"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
